6c3d3da262e2cc641b792b533eacde6921cd2ad5
Manoel do Nascimento <manu3105@yahoo.com>
2023-04-14 00:15:34 -0300

    refactor: base style

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
 
+import surpriseMePrompts from '../../constants'
+
 const Home = () => {
   return (
     <div className='home'>
       <section className='home__search-section'>
-        <p>Start with a detailed description <span>Surprise me</span></p>
+        <p>Start with a detailed description 
+          <span className='home__surprise'>Surprise me</span>
+        </p>
+        <div className="home__input-container">
+          <input placeholder="Insert your prompt here"/>
+          <button>Generate</button>
+        </div>
       </section>
       <section className='home__image-section'>
-        
+
       </section>
     </div>
   )
