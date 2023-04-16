ce59298b9ef01d98c8b147e21d0776f85dd424a8
Manoel do Nascimento <manu3105@yahoo.com>
2023-04-15 23:14:35 -0300

    feat: check iamge size after upload

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,21 +1,34 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './index.scss'
 
 const Modal = ({ setModalOpen, setSelectedImage, selectedImage }) => {
   const [error, setError] = useState(null)
+  const ref = useRef(null)
 
   const closeModal = () => {
     setModalOpen(false)
     setSelectedImage(null)
+  }
+
+  const checkSize = () => {
+    if (ref.current.width == 256 && ref.current.height == 256) {
+
+    } else {
+      setError('Error: choose a 256 x 256 image')
+    }
   }
   return (
     <div className='modal'>
       <div className="modal__container">
         <div onClick={closeModal}>X</div>
         <div className="modal__image-container">
-          {selectedImage && <img src={URL.createObjectURL(selectedImage)} alt='uploaded img'/>}
+          {selectedImage && <img ref={ref} src={URL.createObjectURL(selectedImage)} alt='uploaded img'/>}
         </div>
-        <button>Generate</button>
+        <p>{error || "* Image must be 256 x 256"}</p>
+        {error ?
+          <button onClick={closeModal}>Close</button> :
+          <button onClick={checkSize}>Generate</button> 
+        }
       </div>
     </div>
   )
